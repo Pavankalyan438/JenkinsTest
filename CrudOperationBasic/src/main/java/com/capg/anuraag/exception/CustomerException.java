7cef4982dca9e2bbc95f8ac896e8d03421d9ce62
pavankalyan438 <pavankalyannaini438@gmail.com>
2020-04-13 17:23:39 +0530

    Nojdbc

---
+++ CrudOperationBasic/src/main/java/com/capg/anuraag/exception/CustomerException.java
@@ -0,0 +1,9 @@
+package com.capg.anuraag.exception;
+
+@SuppressWarnings("serial")
+public class CustomerException extends RuntimeException {
+	public CustomerException(String msg) {
+		super(msg);
+	}
+
+}
